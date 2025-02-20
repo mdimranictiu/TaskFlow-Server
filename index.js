c69ef5336b6d7f9b2bbfc7aeb326a7937ace464a
mdimranictiu <imranictiu11@gmail.com>
2025-02-20 20:53:59 +0600

    add task done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const cors=require('cors')
+const jwt = require("jsonwebtoken");
 const app= express();
 const port= process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -19,11 +20,63 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const userCollection = client.db("TaskFlow").collection("users");
+    const tasksCollection = client.db("TaskFlow").collection("tasks");
    
     app.get('/',(req,res)=>{
         res.send('Server is running')
     })
     // backend code
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1hr",
+      });
+      res.send({ token });
+    });
+    // verify Token
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send("Forbidden Access");
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.Access_Token_Secret, (err, decoded) => {
+        if (err) {
+          return res.status(401).send("Forbidden Access");
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // user store
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      // insert email if user doesnt exists:
+      // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      console.log(user);
+      res.send(result);
+    });
+
+    // add task
+
+    app.post('/add-task',verifyToken, async(req,res)=>{
+      const taskData=req.body;
+      const timestamp=  new Date().toISOString().split('T')[0];
+      const newtaskData= {...taskData,timestamp}
+      const result= await tasksCollection.insertOne(newtaskData);
+      res.send(result)
+      console.log(newtaskData)
+    })
     
 
     console.log("Successfully connected to MongoDB!");
@@ -37,3 +90,5 @@ run().catch(console.dir);
 app.listen(port,()=>{
     console.log(`Server is running: http://localhost:${port}`)
 })
+
+console.log()
